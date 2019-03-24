5bf05eee879cbfcd893a9f5a3b0f6c13a1a17b4d
wait <noppakorn@muze.co.th>
2019-03-25 01:34:15 +0700

    feat(caching): add caching for places api

--- services/api/index.js
+++ services/api/index.js
@@ -1,5 +1,4 @@
-import { run } from 'micro'
 import { router } from 'microrouter'
 import places from './controllers/places'
 
-export default (req, res) => run(req, res, router(places))
+export default router(places)
--- services/functions.js
+++ services/functions.js
@@ -1,6 +1,7 @@
+import { run } from 'micro'
 import * as functions from 'firebase-functions'
 import apiHandler from './api'
 import wwwHandler from './www'
 
-export const api = functions.https.onRequest(apiHandler)
-export const www = functions.https.onRequest(wwwHandler)
+export const api = functions.https.onRequest((req, res) => run(req, res, apiHandler))
+export const www = functions.https.onRequest((req, res) => run(req, res, wwwHandler))
